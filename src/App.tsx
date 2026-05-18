c86fb5e8234192ad4017747a74463799b8499631
eeunicey <123467173+eeunicey@users.noreply.github.com>
2026-05-18 20:04:49 +1000

    .

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,12 @@ import heroImg from '@assets/hero.png'
 import '@/App.css'
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
         <>
             <section id="center">
                 <div>
-                    <h1>Get started</h1>
-                    <p>
-                        Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-                    </p>
+                    <h1>Digital Scrapbook</h1> 
                 </div>
-                <button
-                    type="button"
-                    className="counter"
-                    onClick={() => setCount((count) => count + 1)}
-                >
-                    Count is {count}
-                </button>
-            </section>
-            <div className="ticks"></div>
-            <section id="spacer"></section>
-
-            <section>
-
             </section>
         </>
     )
